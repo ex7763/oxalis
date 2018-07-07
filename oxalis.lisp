918f3b4c8c5192c4b93352ad580a0e59538c51fa
ex7763 <eddie55020@gmail.com>
2018-07-08 00:32:42 +0800

    transparent window

--- oxalis.lisp
+++ oxalis.lisp
@@ -6,26 +6,6 @@
 (defvar *window-height* 128)
 (defvar *window-width* 128)
 
-(defclass main-window ()
-  ()
-  (:metaclass qt-class)
-  (:qt-superclass "QWidget"))
-
-(defmethod initialize-instance :after ((instance main-window) &key)
-  (new instance)
-  (let ((figure (make-instance 'figure)))
-
-    (#_resize instance *window-height* *window-width*)
-    ;; (#_setWindowFlags instance (enum-value (#_Qt::FramelessWindowHint)))
-    ;; (#_setAttribute instance (enum-value (#_Qt::WA_TranslucentBackground)))
-    ;; (#_setWindowOpacity instance 0.5)
-    ;; (#_setStyleSheet instance "background-color: rgba(255, 255, 255, 1)")
-    ;; (#_setColor pal (#_QPalette::Background) (#_new QColor 127 127 127 0))
-    ;; (#_setPalette instance pal)
-    
-    (#_setWindowTitle instance "oxalis")
-    ))
-
 (defclass figure ()
   ((m-pressed :initform nil :accessor m-pressed)
    (m-point-x :accessor m-point-x)
@@ -39,15 +19,13 @@
 
 (defmethod initialize-instance :after ((instance figure) &key)
   (new instance)
-  (let ((pal (#_new QPalette (#_palette instance))))
-
-    ;; (#_resize instance *window-height* *window-width*)
-    ;; (#_setWindowFlags instance (enum-value (#_Qt::FramelessWindowHint)))
-    ;; (#_setAttribute instance (enum-value (#_Qt::WA_TranslucentBackground)))
-    (#_setWindowOpacity instance 0.5)
-    ;; (#_setStyleSheet instance "background-color: rgba(255, 255, 255, 1)")
-    ;; (#_setColor pal (#_QPalette::Background) (#_new QColor 127 127 127 0))
-    ;; (#_setPalette instance pal)
+  (let ()
+    (#_setWindowTitle instance "oxalis")
+    (#_resize instance *window-height* *window-width*)
+    ;; 使視窗無邊框、透明
+    (#_setWindowFlags instance (enum-value (#_Qt::FramelessWindowHint)))
+    (#_setAttribute instance (enum-value (#_Qt::WA_TranslucentBackground)))
+    (#_setWindowOpacity instance 1.0)
     ))
 
 (defmethod mouse-press-event ((instance figure) event)
@@ -71,13 +49,12 @@
 (defmethod paint-event ((instance figure) event)
   (let* ((painter (#_new QPainter instance))
          (pixel (#_new QPixmap "/home/eh643027/Project/oxalis/shime1.png")))
-  ;;  (#_setCompositionMode painter (#_QPainter::CompositionMode_Clear))
     (#_drawPixmap painter 0 0 pixel)
     ))
 
 (defun main()
   (make-qapplication)
-  (with-objects ((example (make-instance 'main-window)))
+  (with-objects ((example (make-instance 'figure)))
     (#_show example)
     (#_exec *qapplication*)))
 
